44606131fecd100018359e81b8acb258d9f7a44f
mvaleck <minivaleck@hotmail.com>
2025-03-14 12:08:30 -0300

    projeto

---
+++ aula4/exerc.js
@@ -0,0 +1,10 @@
+var vel = 50
+console.log(`A sua velocidade é ${vel} km/h`);
+
+if (vel >= 80) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+} else {
+    console.log(`OKAY, VOCÊ ESTÁ NO LIMITE DA VELOCIDADE`);
+}
+
+//vamos fazer em forma de site
