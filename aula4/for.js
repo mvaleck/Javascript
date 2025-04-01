30b3e5077aefc1b2df22a71110740bdf119e3883
mvaleck <minivaleck@hotmail.com>
2025-04-01 10:22:54 -0300

    for

---
+++ aula4/for.js
@@ -0,0 +1,9 @@
+const laps = 68;
+
+
+// for (inicialização; condição; atualização)
+for (let index = 1; index <= laps; index += 1) {
+  console.log(`Volta ${index} completada!`);
+}
+
+console.log('Acabou a corrida!');
